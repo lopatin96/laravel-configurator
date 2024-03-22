7dfdfbfe86be58fbfacff07d8b85858a531733f0
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2024-03-22 11:54:24 +0100

    setConfigValue, incrementConfigValue, decrementConfigValue

--- src/Traits/HasConfig.php
+++ src/Traits/HasConfig.php
@@ -15,4 +15,33 @@ trait HasConfig
 
         return ConfiguratorHelper::getLimitedValue($configKey, $this);
     }
+
+    public function setConfigValue(ConfigKey $configKey, $newValue): bool
+    {
+        return $this->forceFill([
+            "config->$configKey->name" => $newValue,
+        ])->save();
+    }
+
+    public function incrementConfigValue(ConfigKey $configKey, int $amount = 1): bool
+    {
+        if ($amount <= 0) {
+            throw new InvalidArgumentException('Amount value passed to increment method must be a positive number.');
+        }
+
+        return $this->forceFill([
+            "config->$configKey->name" => $this->getConfig($configKey) + $amount,
+        ])->save();
+    }
+
+    public function decrementConfigValue(ConfigKey $configKey, int $amount = 1): bool
+    {
+        if ($amount <= 0) {
+            throw new InvalidArgumentException('Amount value passed to increment method must be a positive number.');
+        }
+
+        return $this->forceFill([
+            "config->$configKey->name" => $this->getConfig($configKey) - $amount,
+        ])->save();
+    }
 }
